87505c69c7030e3eed9e847bc4fc1bd25286a92a
Wodman43 <lopezquira10gmail.com>
2023-08-03 18:42:11 -0500

    ola no se nada

---
+++ assets/proyecto_comentarios.js
@@ -0,0 +1,116 @@
+const formulario = document.querySelector('#formulario');
+const listatweets = document.querySelector('#lista-tweets');
+
+
+
+let tweets = [];
+
+// Aqui voy a crear los listeners
+eventlisteners();
+
+function eventlisteners(){
+    formulario.addEventListener('submit', agregartweet);   
+
+    document.addEventListener('DOMContentLoaded', ()=>{
+        tweets = JSON.parse(localStorage.getItem('tweets')) || [];
+        console.log(tweets);
+        crearhtml();
+    })
+   
+
+}
+// aqui van las funciones 
+
+function agregartweet(e){
+    e.preventDefault();
+    const tweet = document.getElementById('tweet');
+    const imagen = tweet.files[0];
+    console.log(tweet);
+    if(!imagen){
+        // alert('no puede estar vacio');
+        mostrarerror('Este campo No puede estar vacio');
+        return;
+    }
+   
+    const tweetobj = {
+        id: Date.now(),
+        imagen: URL.createObjectURL(imagen)
+    }
+
+    tweets = [...tweets, tweetobj];
+ 
+    console.log(tweets);
+    crearhtml();
+    formulario.reset();
+
+}   
+
+function mostrarerror(error){
+    const mensajeerror = document.createElement('p');
+    mensajeerror.textContent = error;
+    mensajeerror.classList.add('error');
+
+    const contenido =document.getElementById('contenido');
+    contenido.appendChild(mensajeerror);
+    setTimeout(()=> {
+        mensajeerror.remove();}, 500);
+    
+
+}
+
+function crearhtml(){ limpiarhtml();
+
+    if(tweets.length > 0){
+        tweets.forEach((imagen) =>{
+            const botonborrar = document.createElement('a');
+            botonborrar.classList= 'borrar-tweet';
+            botonborrar.innerText = 'Eliminar';
+            const corazon = document.createElement('img');
+            corazon.src = 'https://cdn-icons-png.flaticon.com/128/8901/8901364.png'
+            corazon.style.width = '50px';
+            contador = 0;
+            corazon.addEventListener('click',()=>{
+           
+                if(contador === 0){
+                    corazon.src = 'https://cdn-icons-png.flaticon.com/128/8901/8901364.png';
+                    contador=1
+                }else if(contador===1){
+                    corazon.src = 'https://cdn-icons-png.flaticon.com/128/10970/10970401.png';
+                    contador=0
+                }
+                
+            })
+            botonborrar.onclick = () => {
+                borrartweet(imagen.id);
+            }
+            const img = document.createElement('img');
+            img.src = imagen.imagen;
+            img.style.width = '100px'
+            listatweets.appendChild(img);
+
+
+            img.appendChild(botonborrar);
+            listatweets.appendChild(botonborrar);
+            listatweets.appendChild(corazon);
+        })
+    }
+    agregarstorage();
+}
+
+function borrartweet(id){
+    console.log('Eliminando',id)
+    tweets = tweets.filter(imagen => imagen.id!==id);
+    crearhtml();
+}
+
+function limpiarhtml(){
+    while (listatweets.firstChild){
+        listatweets.removeChild(listatweets.firstChild);
+    }
+}
+
+
+function agregarstorage(){
+    localStorage.setItem('tweets',JSON.stringify(tweets));
+}
+
